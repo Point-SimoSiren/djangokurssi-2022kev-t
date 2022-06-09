497522dec884d1f2ac2cca8c926b868788013549
Simo Siren <sirensimo7@gmail.com>
2022-06-09 14:09:28 +0300

    konffattu

--- manage.py
+++ manage.py
@@ -6,7 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'suppliers.settings')
+    # When running on Azure App Service you should use the production settings.
+    settings_module = "suppliers.production" if 'WEBSITE_HOSTNAME' in os.environ else 'suppliers.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
